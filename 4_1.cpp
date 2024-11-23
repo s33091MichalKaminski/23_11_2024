4636d1c5e09522830c2d18afd2cb6e05c82bb073
s33091MichalKaminski <michal.kaminski232@gmail.com>
2024-11-23 14:49:09 +0100

    Update 4_1.cpp

--- 4_1.cpp
+++ 4_1.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Podaj liczbe elementow tablicy"<<endl;
+    cin >> n;
+    int*tab1 = new int[n];
+    cout << "Podaj wartosc liczbowa elementu tablicy" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> tab1[i];
+    }
+    int maxelement = tab1[0];
+    for (int i = 1; i < n; i++) {
+        if (tab1[i] > maxelement) {
+            maxelement = tab1[i];
+        }
+    }
+    cout << "najwiekszy element tablicy to: " << maxelement << endl;
+
+    return 0;
+}
 
